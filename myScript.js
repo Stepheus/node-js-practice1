05a255797c7a935cb3cadcff12aaa296d9ea4770
Stepheus <stepheniboudah@outlook.com>
2024-04-19 19:28:06 -0500

    touchevent added for swipe

--- myScript.js
+++ myScript.js
@@ -6,51 +6,57 @@ function hideMenu(){
     }
 }
 
+function slideSlider(slideLeft, currtSlide){
+    
+    //change text caption too
+    const captions = document.getElementById('box-caption');
+    const currentCaption = captions.querySelector('[data-show="yes"]');
+
+    //Move slider
+    let amountToMove;
+
+    if(slideLeft){
+      amountToMove = "-" + currtSlide.nextElementSibling.dataset.position;
+      currtSlide.nextElementSibling.dataset.selected = "yes";
+      currtSlide.dataset.selected = "no";
+      currentCaption.nextElementSibling.dataset.show ="yes";
+      currentCaption.dataset.show ="no";
+
+    }else{
+        amountToMove = "-" + currtSlide.previousElementSibling.dataset.position;
+        currtSlide.previousElementSibling.dataset.selected = "yes"; 
+        currtSlide.dataset.selected = "no";
+        currentCaption.previousElementSibling.dataset.show="yes";
+        currentCaption.dataset.show="no";
+        
+    }
+    currtSlide.parentElement.style.transform = 'TranslateX('+ amountToMove + "px" + ')';
+    console.log("New Data");
+    console.log("boundingClientRecLeft: " + currtSlide.parentElement.getBoundingClientRect().left);
+}
+
 function slide(direction){
     const slides = document.getElementById('slides');
     const currentSlide = slides.querySelector('[data-selected="yes"]');
-    const captions = document.getElementById('box-caption');
-    const currentCaption = captions.querySelector('[data-show="yes"]');
-    if(direction==="left"){
+    if(direction){
         document.getElementById('leftBttn').style.display= "block";
         if(currentSlide.nextElementSibling){
-            //data-position is updated soon as the doc loads.
-            let amountToMove = "-" + currentSlide.nextElementSibling.dataset.position;
-            currentSlide.parentElement.style.transform = 'TranslateX('+ amountToMove + "px" + ')';
-            //Move anchor to next slide and next text
-            currentSlide.nextElementSibling.dataset.selected = "yes";
-            currentSlide.dataset.selected = "no";
-            currentCaption.nextElementSibling.dataset.show ="yes";
-            currentCaption.dataset.show ="no";
+            slideSlider(direction, currentSlide);
 
             //make button disappear if there's nothing after
             if (!currentSlide.nextElementSibling.nextElementSibling){
                 document.getElementById('rightBttn').style.display= "none";
             }
-            
-        }else{
-            //Nothing
         }
-    }else if(direction==="right"){
+    }else{
         document.getElementById('rightBttn').style.display= "block";
         if(currentSlide.previousElementSibling){
-            // amountToMove = wholeSlideWidth - currentSlide.previousElementSibling.dataset.position;
-            let amountToMove = "-" + currentSlide.previousElementSibling.dataset.position;
-            console.log({amountToMove});
-            currentSlide.parentElement.style.transform = 'TranslateX('+ amountToMove + "px" +')';
-             //Move anchor to next slide and next text
-            currentSlide.previousElementSibling.dataset.selected = "yes"; 
-            currentSlide.dataset.selected = "no";
-            currentCaption.previousElementSibling.dataset.show="yes";
-            currentCaption.dataset.show="no";
+            slideSlider(direction, currentSlide);
 
             //make button disappear if there's nothing before
             if (!currentSlide.previousElementSibling.previousElementSibling){
                 document.getElementById('leftBttn').style.display= "none";
             }
-
-        }else{
-            //Nothing
         }
     }
     
@@ -150,6 +156,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     //Create an array of offsets that will be added as dat inside the html
     //document of each img slide
     const imgSlides = document.getElementsByClassName("slide");
+    console.log("Previous Data");
     let imgArray = Array.from(imgSlides);
     let offset;
     for (let i= 0; i < imgArray.length; i++){
@@ -157,11 +164,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
       imgSlides[i].dataset.position = offset;
     }
 
-     //Service button event(Add it in load event listener)
-    const serviceBttnLeft = document.getElementById('rightBttn');
-    serviceBttnLeft.addEventListener("click", ()=>slide("left"));
-    const serviceBttnRight = document.getElementById('leftBttn');
-    serviceBttnRight.addEventListener("click",()=>slide("right"));
+     //Service click button event
+    const serviceBttnRight = document.getElementById('rightBttn');
+    serviceBttnRight.addEventListener("click", ()=>slide(true));
+
+    const serviceBttnLeft = document.getElementById('leftBttn');
+    serviceBttnLeft.addEventListener("click", ()=>slide(false));
+
+
 
                                 //Intersection Observer
     //Observe richie picture
@@ -216,6 +226,68 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let observer5 = new IntersectionObserver(contactListAppear, option5);
     const listContact = document.getElementById("contact-list");
     observer5.observe(listContact);
+
+    //Touch events variable for swipe
+    /**
+     * Connect phone to laptop
+     */
+    /**
+     I'll need several results
+     *Slides to move when touchstart touch move
+     *Touchend if swipe fast enough. Slide to be moved to next position within
+     a small, maybe 1/4 of amountToMove touchobject.pageX
+     *If not fast enought. I
+         if dist > 1/2 of amountTomove move slide by AmountToMove
+         If not Slide moves back to previous position
+     *
+     */
+    let startX, startY,  distTraveled, timeOfSwipe;
+    const slideBox = document.getElementById("slides");
+    slideBox.addEventListener("touchstart", (e)=>{
+
+        let touchobject = e.changedTouches[0];
+        startX = touchobject.pageX;
+        startY = touchobject.pageY;
+        distTraveled = 0;
+        timeOfSwipe = new Date().getTime();
+        console.log("Touchstart is clicked")
+        e.preventDefault();
+    }, false)
+
+    slideBox.addEventListener("touchmove", (e)=>{
+        console.log("Touch move happening")
+
+        e.preventDefault();
+        
+    }, false)
+
+    //Touch event but with mouse for check
+
+    slideBox.addEventListener("touchend", (e)=>{
+        console.log("touchend over");
+        //if we swipe more than 1/3 the lenght of the picture and within Y distance 
+        //the swipe happens
+        //getclient reco has width
+        let tchobj = e.changedTouches[0];
+        distTraveled = tchobj.pageX - startX;
+        let changeY = tchobj.pageY - startY;
+
+        const secondImg = document.getElementById("kitchen-cover");
+        const moveTreshold  = firstImg.getBoundingClientRect().width/3;
+
+        if((Math.abs(distTraveled) <= moveTreshold) && (changeY <70)){
+            //which direction to swipe
+            if (distTraveled < 0){
+                slide(true);
+            }else{
+                slide(false);
+            }
+        }
+            
+
+
+        e.preventDefault();
+    }, false)
 
 })
 
